abcbdc73b2f4fa6db1f1ed6d1ebaedf5f0897f02
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2017-12-11 15:18:55 +0600

    Add home controller exception handling test

--- src/test/java/com/webpageanalyzer/web/controller/HomeControllerIntegrationTest.java
+++ src/test/java/com/webpageanalyzer/web/controller/HomeControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package com.webpageanalyzer.web.controller;
 import com.webpageanalyzer.WebpageanalyzerApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.webpageanalyzer.web.TestUtils.testGetRequestView;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 /**
  * @since 11/24/17.
@@ -25,5 +28,11 @@ public class HomeControllerIntegrationTest {
     @Test
     public void onGetRequestReturnHomePage() throws Exception {
         testGetRequestView(mockMvc, "/", "form");
+    }
+
+    @Test
+    public void onOnExceptionHandledInExceptionHandler() throws Exception {
+        mockMvc.perform(post("/").param("url", "http://localhost:6523"))
+                .andExpect(model().attributeExists("exception"));
     }
 }
